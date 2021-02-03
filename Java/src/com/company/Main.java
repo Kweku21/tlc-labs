ba14a8716e49ca4130a1205e5ed64e819c854fa6
Emmanuel Debrah <emmanuel.debrah@turntabl.io>
2021-02-03 11:31:22 +0000

    'java'

---
+++ Java/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double rand = Math.random();
+        System.out.println(""+rand);
+    }
+}
